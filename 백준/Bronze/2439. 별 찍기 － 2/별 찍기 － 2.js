28aced916cecd952be381a9bea9c50621d48503a
heon-kim <98210662+heon-kim@users.noreply.github.com>
2022-06-30 01:46:31 +0900

    [Bronze IV] Title: 별 찍기 - 2, Time: 128 ms, Memory: 10136 KB -BaekjoonHub

---
+++ 백준/Bronze/2439. 별 찍기 － 2/별 찍기 － 2.js
@@ -0,0 +1,16 @@
+let fs = require("fs");
+let input = require("fs").readFileSync("/dev/stdin");
+
+let star = "";
+
+for (var i = 1; i <= input; i++) {
+  for (var k = 0; k < input - i; k++) {
+    star += " ";
+  }
+  for (var j = 0; j < i; j++) {
+    star += "*";
+  }
+  star += "\n";
+}
+star = star.slice(0, -1);
+console.log(star);
