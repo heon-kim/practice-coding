1f9e52ec3e94eaa8ac55f422aae4ce6232e15d28
heon-kim <98210662+heon-kim@users.noreply.github.com>
2022-07-01 00:47:02 +0900

    [Bronze V] Title: A+B - 4, Time: 116 ms, Memory: 9348 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.js
@@ -0,0 +1,17 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().split("\n");
+let result = "";
+
+for (let i = 0; i < input.length - 1; i++) {
+  let a = Number(input[i].split(" ")[0]);
+  let b = Number(input[i].split(" ")[1]);
+  let sum = a + b;
+
+  if (!input[i]) {
+    break;
+  } else {
+    result += sum + "\n";
+  }
+}
+
+console.log(result.slice(0, -1));
