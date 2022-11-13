c3ea0bd4ff3a4330e3892a14c936091785db4e78
heon-kim <98210662+heon-kim@users.noreply.github.com>
2022-11-13 22:11:29 +0900

    [Bronze I] Title: 평균은 넘겠지, Time: 140 ms, Memory: 9916 KB -BaekjoonHub

--- 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
+++ 백준/Bronze/4344. 평균은 넘겠지/평균은 넘겠지.js
@@ -12,18 +12,11 @@ rl.on("line", function (line) {
 }).on("close", function () {
   for (let line = 1; line <= input[0]; line++) {
     const testcase = input[line].split(" ");
-    const numberOfScores = testcase[0];
-    let sum = 0;
-    let count = 0;
-    for (let score = 1; score <= numberOfScores; score++) {
-      sum += Number(testcase[score]);
-    }
-    let avg = sum / numberOfScores;
-    for (let score = 1; score <= numberOfScores; score++) {
-      if (testcase[score] > avg) count++;
-    }
-    let answer = (count / numberOfScores) * 100;
-
+    const numberOfScores = testcase.shift();
+    const sum = testcase.reduce((a, b) => +a + +b);
+    const avg = sum / numberOfScores;
+    const count = testcase.filter((score) => score > avg).length;
+    const answer = (count / numberOfScores) * 100;
     console.log(answer.toFixed(3) + "%");
   }
   process.exit();
