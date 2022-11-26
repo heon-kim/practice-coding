7302a3b5dd49648f1cdc28b894d0ed4c6325b1db
heon-kim <98210662+heon-kim@users.noreply.github.com>
2022-11-27 00:02:57 +0900

    [Bronze V] Title: 알파벳 찾기, Time: 160 ms, Memory: 9340 KB -BaekjoonHub

---
+++ 백준/Bronze/10809. 알파벳 찾기/알파벳 찾기.js
@@ -0,0 +1,39 @@
+let fs = require("fs");
+let input = fs.readFileSync("/dev/stdin").toString().trim();
+input = input.split("");
+let alphabet = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
+var answer = Array.from({ length: alphabet.length }, () => -1);
+
+alphabet.forEach((el, index) => {
+  answer[index] = input.findIndex((element) => element === el);
+});
+
+console.log(answer.join(" "));
