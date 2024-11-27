bce0c4d9a35f9e7e9297a1f44f682a2d8f8c01ca
heon-kim <98210662+heon-kim@users.noreply.github.com>
2024-11-27 22:31:31 +0900

    [level 1] Title: 없는 숫자 더하기, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
+++ 프로그래머스/1/86051. 없는 숫자 더하기/없는 숫자 더하기.py
@@ -2,4 +2,7 @@ def solution(numbers):
     # set1 = set([i for i in range(10)])
     # set2 = set(numbers)
     # return sum(list(set1 - set2))
-    return sum(set(range(10))-set(numbers))
+    
+    # return sum(set(range(10))-set(numbers))
+    
+    return 45 - sum(numbers)
