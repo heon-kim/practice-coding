221aa63812734392d448c4dd037d4c1323df1748
heon-kim <98210662+heon-kim@users.noreply.github.com>
2024-11-28 16:37:41 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.05 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -1,11 +1,3 @@
 function solution(n) {
-    let str = ""
-    const repeat = Math.floor(n / 2)
-    for(let i = 0; i< repeat ; i++){
-        str += "수박"
-    }
-    if(n%2 == 1){
-        str += "수"
-    }
-    return str
+    return "수박".repeat(n/2)+"수".repeat(n%2)
 }
