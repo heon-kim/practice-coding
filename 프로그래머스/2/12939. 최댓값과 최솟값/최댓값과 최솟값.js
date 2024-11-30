6576745b1cf0f3d79135633667e0a2a837555270
heon-kim <98210662+heon-kim@users.noreply.github.com>
2024-11-30 23:51:13 +0900

    [level 2] Title: 최댓값과 최솟값, Time: 0.13 ms, Memory: 33.4 MB -BaekjoonHub

--- 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
+++ 프로그래머스/2/12939. 최댓값과 최솟값/최댓값과 최솟값.js
@@ -1,8 +1,4 @@
 function solution(s) {
-    list = s.split(" ").map(i=>parseInt(i))
-    list.sort((x,y)=>{
-        if(x>y) return 1
-        return -1
-    })
-    return list[0]+" "+list[list.length-1]
+    list = s.split(" ")
+    return Math.min(...list)+" "+Math.max(...list)
 }
