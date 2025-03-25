877397559223864b1c8f9ff8d8315bb6cb7f2f0b
heon-kim <98210662+heon-kim@users.noreply.github.com>
2025-03-26 00:50:27 +0900

    [Silver II] Title: 로또, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/6603. 로또/로또.py
+++ 백준/Silver/6603. 로또/로또.py
@@ -1,23 +1,25 @@
-def comb(idx, lev):
-  global choose, arr, k
-  
-  if lev==6:
+def comb(index, level):
+  global n, r, arr, choose
+
+  if(level==r):
     for u in choose:
       print(u, end=" ")
     print()
-    return
-
-  for i in range(idx, k):
+    return 
+  for i in range(index, n):
     choose.append(arr[i])
-    comb(i+1, lev+1)
+    comb(i+1, level+1)
     choose.pop()
 
 while True:
+  I = list(map(int, input().split()))
+  n = I[0]
+  r = 6
+  arr = I[1:]
   choose = []
-  ipt = list(map(int, input().split()))
-  k = ipt[0]
-  arr = ipt[1:]
-  if k == 0:
+
+  if(n==0):
     break
-  comb(0,0)
+
+  comb(0, 0)
   print()
