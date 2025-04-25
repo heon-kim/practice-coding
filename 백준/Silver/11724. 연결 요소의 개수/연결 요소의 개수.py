7efc81a7d22429fee843032e7405e8236095c87c
heon-kim <98210662+heon-kim@users.noreply.github.com>
2025-04-25 23:17:27 +0900

    [Silver II] Title: 연결 요소의 개수, Time: 416 ms, Memory: 119092 KB -BaekjoonHub

--- 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
+++ 백준/Silver/11724. 연결 요소의 개수/연결 요소의 개수.py
@@ -1,31 +1,23 @@
-import sys
-sys.setrecursionlimit(10**6)
-input = sys.stdin.readline
-
-def dfs(idx):
-   global visited, graph
-   visited[idx] = 1
-   for i in range(1, N+1):
-      if not visited[i] and graph[idx][i]:
-         dfs(i)
-
-# MAX = 1000+2
 N, M = map(int, input().split())
-graph = [[0]*(N+1)for _ in range(N+1)]
-visited = [0]*(N+1)
-answer=0
 
-#1. graph에 연결 정보 채우기
+visited = [False]*(N+1)
+arr = [[0]*(N+1) for _ in range(N+1)]
+
 for _ in range(M):
-   u, v = map(int, input().split())
-   graph[u][v] = 1
-   graph[v][u] = 1
+  u, v = map(int, input().split())
+  arr[u][v]=1
+  arr[v][u]=1
 
-#2. DFS 호출
-for i in range(1,N+1):
-   if not visited[i]:
+def dfs(start):
+  visited[start]=True
+  for i in range(1, N+1):
+    if not visited[i] and arr[start][i]==1:
       dfs(i)
-      answer += 1
 
-#3. 출력
-print(answer)
+count = 0
+for i in range(1, N+1):
+  if not visited[i]:
+    dfs(i)
+    count += 1
+
+print(count)
