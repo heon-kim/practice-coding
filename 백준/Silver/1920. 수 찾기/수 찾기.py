fd55f991a14f94a05d1d1bb64de4ed9261d3ea00
heon-kim <98210662+heon-kim@users.noreply.github.com>
2025-04-28 17:03:11 +0900

    [Silver IV] Title: 수 찾기, Time: 156 ms, Memory: 52272 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -1,30 +1,8 @@
-import sys
+N = int(input())
+A = set((map(int, input().split())))
 
-N = int(sys.stdin.readline())
-A = sorted(list(map(int, sys.stdin.readline().split())))
-
-M = int(sys.stdin.readline())
-arr = list(map(int, sys.stdin.readline().split())) # 이 수들이 A안에 존재하는지 알아내기  
-
-def func(num):
-  left = 0
-  right = len(A)-1
-
-  while left <= right:
-    mid = (left+right)//2
-    if A[mid] < num:
-      left = mid + 1
-    elif A[mid] > num:
-      right = mid - 1
-    elif A[mid] == num:
-      return 1
-      
-  return 0
+M = int(input())
+arr = list(map(int, input().split()))
 
 for num in arr:
-  print(func(num))
-
-
-# 오답노트
-# num in A => 시간초과!!
-# sort 후 이진탐색 => 시간초과!!
+  print(1 if num in A else 0)
