648b74381ef125e22d1c3eb372931052dc9ecaaa
heon-kim <98210662+heon-kim@users.noreply.github.com>
2025-05-09 23:19:16 +0900

    [level 2] Title: 가장 큰 수, Time: 91.84 ms, Memory: 46 MB -BaekjoonHub

---
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.js
@@ -0,0 +1,17 @@
+function solution(numbers) {
+    var answer = '';
+    const arr = numbers.map(item => item.toString())
+    const sarr = arr.sort((a, b)=>{
+        if(a+b<b+a){
+            return 1
+        }else if(b+a<a+b){
+            return -1
+        }else{
+            return 0
+        }
+    })
+    if (arr[0] == '0'){
+        return '0'
+    }
+    return sarr.join("");
+}
