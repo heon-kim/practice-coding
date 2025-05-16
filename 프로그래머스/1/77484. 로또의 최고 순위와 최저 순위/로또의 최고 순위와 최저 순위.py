fc84cf2a32ac0951d9240d0c045cb0837f755ed5
heon-kim <98210662+heon-kim@users.noreply.github.com>
2025-05-16 11:51:18 +0900

    [level 1] Title: 로또의 최고 순위와 최저 순위, Time: 0.00 ms, Memory: 9.21 MB -BaekjoonHub

---
+++ 프로그래머스/1/77484. 로또의 최고 순위와 최저 순위/로또의 최고 순위와 최저 순위.py
@@ -0,0 +1,21 @@
+def solution(lottos, win_nums):
+    zero_cnt = 0
+    win_cnt = 0
+    
+    for i in lottos:
+        if i == 0:
+            zero_cnt += 1
+    
+    for i in lottos:
+        if i in win_nums:
+            win_cnt += 1
+    
+    max_rank = 7 - (zero_cnt+win_cnt)
+    min_rank = 7 - win_cnt
+    
+    if(max_rank >= 6):
+        max_rank = 6
+    if(min_rank >= 6):
+        min_rank = 6
+    
+    return [max_rank, min_rank]
